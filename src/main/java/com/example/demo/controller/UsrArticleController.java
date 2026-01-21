7445eb8ab3371e2a2e8f2e455e3a796418a94cc4
korjje617 <korjje617@gmail.com>
2026-01-21 17:20:04 +0900

    modify 구현 + 주석

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -23,8 +23,9 @@ public class UsrArticleController {
 		makeTestData();
 	}
 
-	/// 서비스 메서드------------------------------------------------
-
+	// 서비스 메서드------------------------------------------------
+	
+	// 테스트 데이터 만들기
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
 			String title = "제목 " + i;
@@ -33,7 +34,8 @@ public class UsrArticleController {
 			writeArticle(title, body);
 		}
 	}
-
+	
+	// 파라미터로 게시글 작성
 	private Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
 
@@ -43,28 +45,25 @@ public class UsrArticleController {
 
 		return article;
 	}
-
+	
+	// 리스트에서 id로 게시글 찾기
 	private Article getArticleById(int id) {
 		for (Article article : articles) {
 			if (article.getId() == id) {
-				return article;				
-			}			
+				return article;
+			}
 		}
 		return null;
 	}
-	
-	
-	
-	
-	
 
-	/// 액션 메서드------------------------------------------------
+	// 액션 메서드------------------------------------------------
 
 	// doAdd 주소 요청
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody // 응답
 	public Article doAdd(String title, String body) {
-
+		
+		// 파라미터에서 값 받아서 게시글 쓰기
 		Article article = writeArticle(title, body);
 		return article;
 
@@ -75,20 +74,48 @@ public class UsrArticleController {
 	@ResponseBody // 응답
 	public String doDelete(int id) {
 
-		Article article = getArticleById(id); // 리스트 안에서 파라미터 값으로 찾기
+		// 리스트 안에서 파라미터 값으로 찾기
+		Article article = getArticleById(id);
 		
+		// 만약 없으면 알림
 		if (article == null) {
 			return id + "번 게시글은 없습니다.";
 		}
 		
+		// 있다면 삭제
 		articles.remove(article);
 		return id + "번 게시글이 삭제되었습니다";
+	}
+
+	// doModify 주소 요청
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody // 응답
+	public Object doModify(int id, String title, String body) {
+
+		// 리스트 안에서 파라미터 값으로 찾기
+		Article article = getArticleById(id);
+		
+		// 만약 없으면 알림
+		if (article == null) {
+			return id + "번 게시글은 없습니다.";
+		}
+		
+		// 있다면 파라미터에서 값 받아서 수정
+		article.setTitle(title);
+		article.setBody(body);
+		
+		return id + "번 게시글이 수정되었습니다<br>" + " 수정된 게시글 : " + article;
+		// 가독성이 별로여서 개행하려고 했는데 \n 이 안먹혔다
+		// 그래서 검색해보니까 <br> 쓰라고 해서 써봤더니 정상 적용
 	}
 
 	// getArticles 주소 요청
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody // 응답
+
 	public List<Article> getArticles() {
+		// 게시글 리스트 출력
 		return articles;
 	}
+
 }
