026fda4473b43c148bd96a9c4f89b4476de29f43
korjje617 <korjje617@gmail.com>
2026-01-23 16:33:22 +0900

    세션으로 로그아웃 구현

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -20,8 +20,9 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
-		
+	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+			String email) {
+
 		if (Ut.isEmptyOrNull(loginId)) {
 			return ResultData.from("F-1", "loginId 입력");
 		}
@@ -52,99 +53,87 @@ public class UsrMemberController {
 		return ResultData.newData(doJoinRd, member);
 	}
 
-	
 	@RequestMapping("/usr/member/doLogIn")
 	@ResponseBody
 	public ResultData<Member> doLogin(HttpSession session, String loginId, String loginPw) {
-		
+
 		// 기본 로그아웃 상태
 		boolean isLogined = false;
-		
+
 		// 로그인된 멤버 아이디가 널값이 아니라면
 		if (session.getAttribute("loginedMemberId") != null) {
 			// 로그인 상태
 			isLogined = true;
 		}
-		
+
 		// 이미 로그인 된 상태라면
 		if (isLogined) {
 			// 알려준다
-			return ResultData.from("F-A", "로그인 중");
+			return ResultData.from("F-A", "이미 로그인 중");
 		}
-		
-		// 입력된 값들이  null 인지 확인하라고 유틸에게 요청
+
+		// 입력된 값들이 null 인지 확인하라고 유틸에게 요청
 		if (Ut.isEmptyOrNull(loginId)) {
 			return ResultData.from("F-1", "아이디 작성해");
 		}
 		if (Ut.isEmptyOrNull(loginPw)) {
 			return ResultData.from("F-2", "비밀번호 작성해");
 		}
-		
+
 		// 이 아이디를 가진 객체 가져오라고 요청
 		Member member = memberService.getMemberByLoginId(loginId);
 
-		
 		// 틀렸을 때
 		if (member == null) {
 			return ResultData.from("F-3", Ut.f("%s 는 없는 아이디", loginId));
 		}
-		
+
 		if (member.getLoginPw().equals(loginPw) == false) {
 			return ResultData.from("F-4", "틀린 비밀번호");
 		}
-		
+
 		session.setAttribute("loginedMemberId", member.getId());
-		
+
 		// 위 과정을 다 통과해야 성공
 		return ResultData.from("S-1", Ut.f("%s 님 로그인", member.getNickname()), member);
-		
+
 	}
-	
+
 	@RequestMapping("/usr/member/doLogOut")
 	@ResponseBody
 	public ResultData<Member> doLogOut(HttpSession session, String loginId, String loginPw) {
-		
-		// 기본 로그인 상태
+		// 기본 로그아웃 상태
 		boolean isLogined = false;
-		
-		// 로그인된 멤버 아이디가 널값이 아니라면
-		if (session.getAttribute("loginedMemberId") != null) {
-			// 로그인 상태
-			isLogined = true;
+
+		// 로그인된 멤버 아이디가 널값이라면
+		if (session.getAttribute("loginedMemberId") == null) {
+			// 이미 로그아웃 상태
+			return ResultData.from("F-A", "이미 로그아웃");
 		}
-		
+
 		// 이미 로그인 된 상태라면
 		if (isLogined) {
 			// 알려준다
-			return ResultData.from("F-A", "로그인 중");
+			return ResultData.from("F-A", "이미 로그아웃");
 		}
-		
-		// 입력된 값들이  null 인지 확인하라고 유틸에게 요청
-		if (Ut.isEmptyOrNull(loginId)) {
-			return ResultData.from("F-1", "아이디 작성해");
-		}
-		if (Ut.isEmptyOrNull(loginPw)) {
-			return ResultData.from("F-2", "비밀번호 작성해");
-		}
-		
+
 		// 이 아이디를 가진 객체 가져오라고 요청
 		Member member = memberService.getMemberByLoginId(loginId);
 
-		
 		// 틀렸을 때
 		if (member == null) {
 			return ResultData.from("F-3", Ut.f("%s 는 없는 아이디", loginId));
 		}
-		
+
 		if (member.getLoginPw().equals(loginPw) == false) {
 			return ResultData.from("F-4", "틀린 비밀번호");
 		}
 		
-		// 위 과정을 다 통과해야 성공
-		return ResultData.from("S-1", Ut.f("%s 님 로그인", member.getNickname()), member);
-		
-	}
+		session.setAttribute("loginedMemberId", null);
 
-}
+		// 위 과정을 다 통과해야 성공
+		return ResultData.from("S-1", Ut.f("%s 님 로그아웃", member.getNickname()), member);
+
+	}
 
 }
